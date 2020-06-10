febd499f42ef45674e2d2933f46cd1957d5b00aa
vagnerwentz <wentz.vagner@gmail.com>
2020-06-10 16:07:40 -0300

    Applying the SOLID method to separate the services.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 /* Repository */
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/* Service */
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
@@ -17,26 +20,24 @@ appointmentsRouter.get('/', (request, response) => {
 
 /* Create an appointment */
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked!' });
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
