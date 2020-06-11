64458f25cde527e6f57d1fe1bf3a55129120372e
vagnerwentz <wentz.vagner@gmail.com>
2020-06-11 19:02:18 -0300

    Added the AppError.

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+/* Authenticate config */
 import authConfig from '../config/auth';
+
+/* Error */
+import AppError from '../errors/AppError';
 
 interface TokenPayload {
   iat: number;
@@ -17,7 +21,7 @@ export default function ensureAuthenticated(
   /* Validating the JWT Token */
   const authHeader = request.headers.authorization;
   if (!authHeader) {
-    throw new Error('JWT Token is missing.');
+    throw new AppError('JWT Token is missing.', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -32,6 +36,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch {
-    throw new Error('Invalid JWT Token.');
+    throw new AppError('Invalid JWT Token.', 401);
   }
 }
