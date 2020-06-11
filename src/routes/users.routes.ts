b228ae4265dad08c303a9e93b390e972d8d414f4
vagnerwentz <wentz.vagner@gmail.com>
2020-06-11 18:43:54 -0300

    Added the feature to save the avatar into database.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,7 @@ import uploadConfig from '../config/upload';
 
 /* Service */
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 /* Middleware */
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -40,7 +41,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.status(200).json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.status(200).json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
