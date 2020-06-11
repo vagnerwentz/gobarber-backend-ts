723ad248b8cd79dddf90d7017a9e763f0059451e
vagnerwentz <wentz.vagner@gmail.com>
2020-06-11 18:43:16 -0300

    Update avatar service.

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import fs from 'fs';
+import { getRepository } from 'typeorm';
+
+/* Upload config */
+import uploadConfig from '../config/upload';
+
+/* Model */
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated user can change avatar.');
+    }
+
+    if (user.avatar) {
+      /* Delete the previous avatar */
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
