def649a6775d062722e2f54cad456e499544a5cc
vagnerwentz <wentz.vagner@gmail.com>
2020-06-12 17:10:45 -0300

    Added the function to handling errors.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
-import express from 'express';
 import routes from './routes';
 
+/* Upload file config */
 import uploadConfig from './config/upload';
+
+/* Error */
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -12,6 +17,22 @@ app.use(express.json());
 
 app.use(routes);
 app.use('/files', express.static(uploadConfig.directory));
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
