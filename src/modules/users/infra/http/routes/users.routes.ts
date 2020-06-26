59c0ffa533d01bd53dfe62a1b52bfd2bb1f2b3fe
vagnerwentz <wentz.vagner@gmail.com>
2020-06-26 17:16:48 -0300

    feat: Imported UsersRepository and moved the instance inside the routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -8,6 +8,9 @@ import uploadConfig from '@config/upload';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
+/* Repository */
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
 /* Middleware */
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -18,7 +21,8 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const usersRepository = new UsersRepository();
+  const createUser = new CreateUserService(usersRepository);
 
   const user = await createUser.execute({
     name,
@@ -37,7 +41,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
