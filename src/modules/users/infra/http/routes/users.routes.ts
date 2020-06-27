e23176a2acd3fc82e536de50c6d07082388feea6
vagnerwentz <wentz.vagner@gmail.com>
2020-06-27 15:24:20 -0300

    feat: Resolve CreateUserService and UpdateUserAvatarService

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { container } from 'tsyringe';
 
 /* Upload */
 import uploadConfig from '@config/upload';
@@ -7,9 +8,6 @@ import uploadConfig from '@config/upload';
 /* Service */
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
-/* Repository */
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 /* Middleware */
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -21,8 +19,7 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
+  const createUser = container.resolve(CreateUserService);
 
   const user = await createUser.execute({
     name,
@@ -41,8 +38,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
