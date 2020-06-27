5a1f67224d887df0ab3773d8b4b45395845dd73b
vagnerwentz <wentz.vagner@gmail.com>
2020-06-27 15:48:28 -0300

    feat: Moved the business rule to SessionsController

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,22 +1,12 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-/* Service */
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
 
+const sessionsController = new SessionsController();
+
 /* Create a session */
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({ email, password });
-
-  delete user.password;
-
-  return response.status(200).json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
