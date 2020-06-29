11a7299f8879fd8f5567319bc5d758ba79908896
vagnerwentz <wentz.vagner@gmail.com>
2020-06-29 14:17:21 -0300

    test: Create Appointment test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
-});
+/* Fake */
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+/* Service */
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      provider_id: '1212',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1212');
+
+  });
+
+  // it('should not be able to create two appointment in the same time', () => {
+  //   expect(1 + 2).toBe(4)
+  // })
+})
