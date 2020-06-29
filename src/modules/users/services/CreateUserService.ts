8fb1daa1c31dad1628fd7b0f2889971c9ac826b3
vagnerwentz <wentz.vagner@gmail.com>
2020-06-29 16:50:35 -0300

    feat: Imported IHashProvider, inject HashProvider and used hashProvider to generate the hash

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,3 @@
-import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 /* Error */
@@ -9,6 +8,8 @@ import User from '../infra/typeorm/entities/User';
 
 /* Repository */
 import IUsersRepository from '../repositories/IUsersRepository';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   name: string;
@@ -21,6 +22,9 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -30,7 +34,7 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
