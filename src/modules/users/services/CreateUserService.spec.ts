f0f93c25ba107e01c4fa9f8ab9e7b76c01a5df4e
vagnerwentz <wentz.vagner@gmail.com>
2020-06-29 16:02:46 -0300

    test: Create a new User and not be able to create with same email from another

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,42 @@
+/* Error */
+import AppError from '@shared/errors/AppError';
+
+/* Fake */
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+/* Service */
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  });
+});
