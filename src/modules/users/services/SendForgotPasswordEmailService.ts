88b06a4443a70ab1f3faa0448c1f2b0677e6f57f
vagnerwentz <wentz.vagner@gmail.com>
2020-06-30 12:02:22 -0300

    feat: Service to send forgot password to email

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,37 @@
+import { inject, injectable } from 'tsyringe';
+
+/* Error */
+// import AppError from '@shared/errors/AppError';
+
+/* Model */
+// import User from '../infra/typeorm/entities/User';
+
+/* Provider */
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+/* Repository */
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
