c511d08dc18cfd31306cc59c8445658752bd4c12
vagnerwentz <wentz.vagner@gmail.com>
2020-06-30 18:41:09 -0300

    fix: ESLint

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,16 +1,18 @@
 /* Fake */
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
 /* Service */
 import CreateAppointmentService from './CreateAppointmentService';
 
 /* Error */
-import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
     const appointment = await createAppointment.execute({
       provider_id: '1212',
@@ -19,12 +21,13 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('1212');
-
   });
 
   it('should not be able to create two appointment in the same time', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
     const appointmentDate = new Date(2020, 4, 10, 11);
 
@@ -33,9 +36,11 @@ describe('CreateAppointment', () => {
       date: appointmentDate,
     });
 
-    expect(createAppointment.execute({
-      provider_id: '1212',
-      date: appointmentDate,
-    })).rejects.toBeInstanceOf(AppError);
-  })
-})
+    await expect(
+      createAppointment.execute({
+        provider_id: '1212',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
