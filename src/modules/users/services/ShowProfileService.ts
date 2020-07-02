c93899a78d126692040830605fb673b330864959
vagnerwentz <wentz.vagner@gmail.com>
2020-07-02 15:54:01 -0300

    feat: Show Profile Service

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from 'tsyringe';
+
+/* Error */
+import AppError from '@shared/errors/AppError';
+
+/* Model */
+import User from '../infra/typeorm/entities/User';
+
+/* Repository */
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
