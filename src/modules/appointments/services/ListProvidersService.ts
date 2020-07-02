60acc6d7d77fe3586f29edd6ebd11c988dbcaede
vagnerwentz <wentz.vagner@gmail.com>
2020-07-02 17:29:06 -0300

    feat: ListProvidersService

---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from 'tsyringe';
+
+/* Repository */
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+/* Model */
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return users;
+  }
+}
+
+export default ListProvidersService;
