591308b04f2dd79fd546382d7bf4e1ad4569a293
vagnerwentz <wentz.vagner@gmail.com>
2020-07-04 17:35:56 -0300

    feat: Route to show all appointments on a specific day

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -5,14 +5,19 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 
 /* Middleware */
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 /* Create an appointment */
 appointmentsRouter.post('/', appointmentsController.create);
+
+/* List appointments with provider logged */
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
