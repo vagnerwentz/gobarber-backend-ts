ddbee3163ef6439c57d10cb12cee3e0ac67b9f71
vagnerwentz <wentz.vagner@gmail.com>
2020-07-14 18:18:55 -0300

    feat: eslint fixed the code

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -42,7 +42,10 @@ class AuthenticateUserService {
     }
 
     /* If user was found we will compare the passwords */
-    const passwordMatched = await this.hashProvider.compareHash(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
