09b5d39348c3f1643235221edc0b95c72884bbd8
vagnerwentz <wentz.vagner@gmail.com>
2020-07-15 15:01:10 -0300

    feat: Changed the body to query where we take the dates

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -7,17 +7,17 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
-    const listPrviderAppointments = container.resolve(
+    const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
     );
 
-    const appointments = await listPrviderAppointments.execute({
+    const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
