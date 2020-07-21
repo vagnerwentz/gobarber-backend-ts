a65620ae55d3ea435615e1501b5f654c20f0e25b
vagnerwentz <wentz.vagner@gmail.com>
2020-07-21 10:08:17 -0300

    feat: rateLimiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -21,12 +21,12 @@ import '@shared/infra/typeorm';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 
 app.use(errors());
 
